ce4b9188798c815d6c9f46cf4f2db8748d3eec31
Benson Tamunoemi <bensontamunoemi@gmail.com>
2022-11-20 23:34:56 +0400

    added debugger and patch request

--- app.js
+++ app.js
@@ -40,6 +40,15 @@ app.post('/api/v1/tours', (req, res) => {
   });
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here>',
+    },
+  });
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
