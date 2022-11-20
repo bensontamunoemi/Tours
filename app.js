c47ec4cbd1964923979705549979e3761ed86439
Benson Tamunoemi <bensontamunoemi@gmail.com>
2022-11-20 21:14:02 +0400

    initial setup

---
+++ app.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
+const __dirname = path.resolve();
+
+const app = express();
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+
+  const tour = tours.find((el) => el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  res.status(200).json({
+    status: 'success',
+  });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
