2ceba8421eab26dee0783d87b35a6d2a3a431e9e
Benson Tamunoemi <bensontamunoemi@gmail.com>
2022-11-26 22:43:18 +0400

    more fields on tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,15 +5,52 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour name must be provided'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour duration must be provided'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour max group size must be provided'],
+  },
+  difficultly: {
+    type: String,
+    required: [true, 'A tour difficulty must be provided'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuality: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour price must be provided'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour summary must be provided'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour image cover must be provided'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
