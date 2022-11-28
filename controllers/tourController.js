c70aa01f3ca709eec78c53193335843a880862a7
Benson Tamunoemi <bensontamunoemi@gmail.com>
2022-11-28 21:45:10 +0400

    implemented pagination, filtering and sortin

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -10,29 +10,57 @@ const __dirname = path.resolve();
 const getAllTours = async (req, res) => {
   try {
     // Build the query
-    // 1.) Filtering
+    // 1A) Filtering
     const objectQuery = { ...req.query };
     const excludedFields = ['page', 'limit', 'sort', 'fields'];
     excludedFields.forEach((el) => {
       delete objectQuery[el];
     });
 
-    // 1.) Advanced Filtering
+    // 1B) Advanced Filtering
     let queryStr = JSON.stringify(objectQuery);
 
     queryStr = queryStr.replace(/\b(gte|gt|lt|lte)\b/g, (match) => `$${match}`); // finds gte, gt, le, lte and adds $ to them
     // gte, gt, le, lte  for mongoDb to add the dollar sign $ so mongoDb can recognize the operator when received from the req.query
-
-    console.log('Hello', JSON.parse(queryStr));
 
     /** @type {*}
      *  @private                            \|/ the below shows the greater than operator how it is passed as a query
      * 127.0.0.1:3000/api/v1/tours?duration[gte]=5&difficulty=easy&page=2&limit=5
      */
 
-    const query = Tour.find(JSON.parse(queryStr)).lean().exec();
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      // sort("-price -ratingsAverage")
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // 3) Fields limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v'); // the will exclude __v from sending to client
+    }
+
+    // 4) Pagination
+
+    const page = +req.query.page || 1;
+    const limit = +req.query.limit || 100;
+    const skip = (page - 1) * limit; // all results that comes before the page we are requesting
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numOfTours = await Tour.countDocuments();
+      if (skip >= numOfTours) throw new Error('This page does not exist');
+    }
+
     // Execute the query
-    const tours = await query;
+    const tours = await query.lean().exec();
 
     // Send response to client
     res.status(200).json({
