ed4e6f1bc071313438a1dfba7d15aef59e443edb
Benson Tamunoemi <bensontamunoemi@gmail.com>
2022-11-30 23:06:42 +0400

    get tour statistics

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -113,6 +113,41 @@ const deleteTour = async (req, res) => {
   }
 };
 
+const getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: {
+          avgPrice: 1,
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: stats,
+    });
+  } catch {
+    res.status(404).json({
+      status: 'fail',
+    });
+  }
+};
+
 export {
   getAllTours,
   getTour,
@@ -120,4 +155,5 @@ export {
   updateTour,
   deleteTour,
   aliasTopTours,
+  getTourStats,
 };
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,13 +5,14 @@ import {
   getAllTours,
   getTour,
   updateTour,
-  aliasTopTours, // manipulate the req.query so the client does not have to
+  aliasTopTours,
+  getTourStats, // manipulate the req.query so the client does not have to
 } from '../controllers/tourController.js';
 
 const router = express.Router();
 
 // router.param('id', checkID);
-
+router.route('/tour-stats').get(getTourStats);
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
