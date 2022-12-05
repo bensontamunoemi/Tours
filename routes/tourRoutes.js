ef949915c00f36adb2b50edf414394bc3fc958c1
Benson Tamunoemi <bensontamunoemi@gmail.com>
2022-12-05 21:20:42 +0400

    getMonthlyPlan statistics

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,13 +6,15 @@ import {
   getTour,
   updateTour,
   aliasTopTours,
-  getTourStats, // manipulate the req.query so the client does not have to
+  getTourStats,
+  getMonthlyPlan, // manipulate the req.query so the client does not have to
 } from '../controllers/tourController.js';
 
 const router = express.Router();
 
 // router.param('id', checkID);
 router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
